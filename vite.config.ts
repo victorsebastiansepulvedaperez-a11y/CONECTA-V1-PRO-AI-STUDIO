f6de0dc39daae1595f3bfea94d3bec1317a52c7e
victorsebastiansepulvedaperez-a11y <victorsebastiansepulvedaperez@gmail.com>
2026-07-19 00:52:25 -0400

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // Si usas React. Si usas Vanilla JS, puedes borrar esta línea.
 
 export default defineConfig({
-  base: './','/CONECTA-V1-PRO-AI-STUDIO/',
+  base: '/CONECTA-V1-PRO-AI-STUDIO/',
   plugins: [react()] // Si borraste la línea de arriba, borra también esta línea de plugins.
 })
